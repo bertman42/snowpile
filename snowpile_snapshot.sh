a1b3244474639f5629d76277d78a220568665556
bertman42 <bertman30@msn.com>
2017-09-04 20:55:19 +0000

    Updated and all working - just need to cron it

--- snowpile_snapshot.sh
+++ snowpile_snapshot.sh
@@ -1,8 +1,15 @@
 #!/bin/bash
+#
+# Setup variables
+TIMELAPSE_VIDEO=snowpile_timelapse.mp4
+REMOTESERVER="tsttoolsserver"
+REMOTEPATH="/var/www/html/snowpile_timelapse"
+
+
 #Let's make sure we have the right TimeZone
 export TZ=America/Toronto
 
-DIR='/home/pi/snowpile/images_timelapse/'
+DIR='/home/pi/snowpile/images_timelapse'
 echo -e "\nDIR is: "$DIR
 
 typeset -i N=$(cat timerfile.dat)
@@ -21,18 +28,17 @@ echo -e "\nFILENAME is: " $FILENAME
 
 echo -e "\nTaking picture..."
 
-#/usr/bin/fswebcam -d /dev/video0 -i 0 -S 60 -F 20 --no-banner --deinterlace -r 1024x768 $DIR$FILENAME
+/usr/bin/fswebcam -d /dev/video0 -i 0 -S 60 -F 20 --no-banner --deinterlace -r 1024x768 $DIR/$FILENAME
 
-/usr/bin/fswebcam -d /dev/video0 -i 0 -S 30 -F 5 --no-banner --deinterlace -r 1024x768 $DIR$FILENAME
+#/usr/bin/fswebcam -d /dev/video0 -i 0 -S 30 -F 5 --no-banner --deinterlace -r 1024x768 $DIR$FILENAME
 
 echo -e "\nAdding banner..."
-convert $DIR$FILENAME -pointsize 30 -fill white -stroke black -gravity southeast -annotate +4+0 "$(date +"%D %r")" $DIR$FILENAME
+#convert $DIR/$FILENAME -pointsize 30 -fill white -stroke black -gravity southeast -annotate +4+0 "$(date +"%D %r")" $DIR$FILENAME
+convert $DIR/$FILENAME -pointsize 40 -fill white -undercolor '#00000080' -stroke black -gravity southeast -draw "text 20,0 '$(date +"%D")' " $DIR/$FILENAME
 
 echo -e "\nMaking the timelapse..."
+avconv -y -r 5 -i $DIR/%05d.jpg -vcodec libx264 -r 30 $DIR/$TIMELAPSE_VIDEO
 
-#cd $DIR
-
-avconv -y -r 1 -i $DIR%05d.jpg -vcodec libx264 -r 1 $DIR/output.mp4
-
-cd ~
-
+echo  -e "\n Transferrring to server... "
+scp $DIR/$FILENAME test.lab@$REMOTESERVER:$REMOTEPATH
+scp $DIR/$TIMELAPSE_VIDEO test.lab@$REMOTESERVER:$REMOTEPATH
