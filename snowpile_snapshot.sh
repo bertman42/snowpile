be92633a03da6e992a52ede3878946fa5ca41eff
bertman42 <bertman30@msn.com>
2017-09-04 15:37:34 +0000

    Updated for timerfile.dat and now stitching video together

--- snowpile_snapshot.sh
+++ snowpile_snapshot.sh
@@ -1,14 +1,36 @@
+#!/bin/bash
+
 DIR='/home/pi/snowpile/images/'
-echo "DIR is: "$DIR
+echo -e "\nDIR is: "$DIR
 
-FILENAME=`date | sed -e 's/ /_/g' | sed -e 's/:/_/g'.jpg`
-echo "FILENAME is: $FILENAME
+typeset -i N=$(cat timerfile.dat)
+echo -e "\nOriginal number is: " $N
 
-echo "Taking picture..."
-#/usr/bin/fswebcam -d /dev/video0 -i 0 -S 60 -F 20 --deinterlace -r 1024x768 $DIR$FILENAME
-/usr/bin/fswebcam -d /dev/video0 -i 0 -S 5 -F 1 --deinterlace --no-banner -r 1024x768 $DIR$FILENAME
+N=$N+1
+echo -e "\nNew number is: " $N
+echo $N > /home/pi/snowpile/timerfile.dat
 
-echo 'Adding banner..."
+NUMBER=$(printf "%05d.jpg" "$N")
+
+echo -e "\nNew number padded is: " $NUMBER
+
+FILENAME=$NUMBER
+echo -e "\nFILENAME is: " $FILENAME
+
+echo -e "\nTaking picture..."
+
+#/usr/bin/fswebcam -d /dev/video0 -i 0 -S 60 -F 20 --no-banner --deinterlace -r 1024x768 $DIR$FILENAME
+
+/usr/bin/fswebcam -d /dev/video0 -i 0 -S 30 -F 5 --no-banner --deinterlace -r 1024x768 $DIR$FILENAME
+
+echo -e "\nAdding banner..."
 convert $DIR$FILENAME -pointsize 30 -fill white -stroke black -gravity southeast -annotate +4+0 "$(date +"%D %r")" $DIR$FILENAME
 
+echo -e "\nMaking the timelapse..."
+
+#cd $DIR
+
+avconv -y -r 1 -i $DIR/%05d.jpg -vcodec libx264 -r 1 $DIR/output.mp4
+
+#cd ~
 
