3875dd308b1be25009bfbeba16f196d44e901cce
bertman42 <bertman30@msn.com>
2017-09-04 00:35:25 +0000

    Updates with filename and dir names

--- snowpile_snapshot.sh
+++ snowpile_snapshot.sh
@@ -1,7 +1,14 @@
-timestamp=`date | sed -e 's/ /_/g' | sed -e 's/:/_/g'`
+DIR='/home/pi/snowpile/images/'
+echo "DIR is: "$DIR
 
-/usr/bin/fswebcam -d /dev/video0 -i 0 -S 60 -F 20 --deinterlace -r 1024x768 $timestamp.jpg
+FILENAME=`date | sed -e 's/ /_/g' | sed -e 's/:/_/g'.jpg`
+echo "FILENAME is: $FILENAME
 
-#/usr/bin/fswebcam -d /dev/video0 -i 0 -S 5 -F 1 --deinterlace -r 1024x768 $timestamp.jpg
+echo "Taking picture..."
+#/usr/bin/fswebcam -d /dev/video0 -i 0 -S 60 -F 20 --deinterlace -r 1024x768 $DIR$FILENAME
+/usr/bin/fswebcam -d /dev/video0 -i 0 -S 5 -F 1 --deinterlace --no-banner -r 1024x768 $DIR$FILENAME
+
+echo 'Adding banner..."
+convert $DIR$FILENAME -pointsize 30 -fill white -stroke black -gravity southeast -annotate +4+0 "$(date +"%D %r")" $DIR$FILENAME
 
 
